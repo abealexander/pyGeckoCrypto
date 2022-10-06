93d409480b52ad3ba40410a60961bcfe053ebbc9
abealexander07 <abealexander07@gmail.com>
2022-10-06 17:11:35 +0530

    Integration Test added

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,5 +1,5 @@
 import pytest
-from pyGeckoCrypto import get_current_price
+from pyGeckoCrypto import get_current_price, get_hist_price_range
 
 good_price_data = [
     ("bitcoin", "usd", float),
@@ -11,6 +11,20 @@ good_price_data = [
 
 bad_price_data = [("btc", "usd"), ("ethereum", "dollar")]
 
+good_price_hist_data = [
+    ("bitcoin", "usd", "1664582400", "1665052399", dict),
+    ("ethereum", "inr", "1640995200", "1643673599", dict),
+    ("fantom", "usd", "1234", "1635724800", dict),
+    ("optimism", "eur", "1625097600", "1627775999", dict),
+]
+
+bad_price_hist_data = [
+    ("btc", "usd", "166458240O", "1665052399"),
+    ("ethereum", "dollar", "1640995200", "1643673599"),
+    ("fantom", "usd", "1AB234", "1635724800"),
+    ("optimism", "eur", "1625097600", "162777--5999"),
+]
+
 
 @pytest.mark.parametrize("coinID, currency, response", good_price_data)
 def test_get_current_price_good(coinID, currency, response):
@@ -21,3 +35,16 @@ def test_get_current_price_good(coinID, currency, response):
 def test_get_current_price_bad(coinID, currency):
     with pytest.raises(Exception):
         get_current_price(coinID, currency)
+
+
+@pytest.mark.parametrize(
+    "coinID, currency, startdate, enddate, response", good_price_hist_data
+)
+def test_get_hist_price_range_good(coinID, currency, startdate, enddate, response):
+    assert type(get_hist_price_range(coinID, currency, startdate, enddate)) == response
+
+
+@pytest.mark.parametrize("coinID, currency, startdate, enddate", bad_price_hist_data)
+def test_get_hist_price_range_bad(coinID, currency, startdate, enddate):
+    with pytest.raises(Exception):
+        get_hist_price_range(coinID, currency, startdate, enddate)
